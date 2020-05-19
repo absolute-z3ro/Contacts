39e87ec7c4764326e69a8ace2916a26c46004e8e
Shahnawaz Ansari <absolutez3ro.exe@gmail.com>
2020-05-19 12:09:18 +0530

    Move query inside ContactLiveData

--- app/src/main/java/xyz/absolutez3ro/contacts/data/ContactLiveData.kt
+++ app/src/main/java/xyz/absolutez3ro/contacts/data/ContactLiveData.kt
@@ -1,11 +1,42 @@
 package xyz.absolutez3ro.contacts.data
 
 import android.content.Context
+import android.provider.ContactsContract
 
 class ContactLiveData(private val context: Context) :
-    ContentProviderLiveData<List<Contact>>(context, Repository.URI) {
+    ContentProviderLiveData<List<Contact>>(context, URI) {
 
-    private val repository = Repository.getInstance()
+    companion object {
+        val URI = ContactsContract.Contacts.CONTENT_URI
+    }
 
-    override fun getContentProviderValue() = repository.getContacts(context)
+    private fun getContacts(context: Context): List<Contact> {
+        val listOfContacts = mutableListOf<Contact>()
+        val projection = arrayOf(
+            ContactsContract.Contacts._ID,
+            ContactsContract.Contacts.DISPLAY_NAME
+        )
+
+        val cursor = context.contentResolver.query(
+            URI,
+            projection,
+            null,
+            null,
+            ContactsContract.Contacts.DISPLAY_NAME
+        )
+        if (cursor != null && cursor.moveToFirst()) {
+            do {
+                val id = cursor.getLong(0)
+                val name = cursor.getString(1)
+
+                listOfContacts.add(Contact(id, name))
+            } while (cursor.moveToNext())
+
+            cursor.close()
+        }
+
+        return listOfContacts
+    }
+
+    override fun getContentProviderValue() = getContacts(context)
 }
